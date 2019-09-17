6295f8997a6c1c9459abe7961df08e3a18b73821
Evelyn <eduarte019@west-mec.org>
2019-09-16 17:36:33 -0700

    Error
    syntax error

--- 02-AJAX/Exercise-LAMP/www/scripts.js
+++ 02-AJAX/Exercise-LAMP/www/scripts.js
@@ -1,8 +1,8 @@
 /*  Exercise 01_11_01
 
     Whole Spectrum Energy Solutions
-    Author: 
-    Date:   
+    Author: Evelyn Duarte
+    Date:  09/12/19 
 
     Filename: script.js
 */
@@ -12,6 +12,20 @@
 // global variables
 var selectedCity = "Tucson, AZ";
 var weatherReport;
+var httpRequest = false;
+
+function getRequestObject() {
+   // alert('to annoy');
+   try {
+      httpRequest = new XMLHttpRequest(); 
+   }
+   catch (requestError) {
+       document.querySelector("p.error").innerHTML = "Forecast not supported by your browser.";
+       document.querySelector("p.error").style.display = "block";
+       return false;
+   }
+   return httpRequest;
+}
 
 function getWeather(evt) {
    var latitude;
@@ -32,6 +46,19 @@ function getWeather(evt) {
    } else if (selectedCity === "Montreal, QC") {
       latitude = 45.5601062;
       longitude = -73.7120832;
+   }
+   if (!httpRequest) {
+      httpRequest = getRequestObject()
+   };
+   httpRequest.abort();
+   httpRequest.open("get", "solar.php?" + "lat=" + latitude + "&lng" + longitude, true);
+   httpRequest.send(null);
+   httpRequest.onreadystatechange = fillWeather;
+}
+function fillWeather(){
+   if (httpRequest.readyState === 4 &&
+      httpRequest.status === 200) {
+     weatherReport = JSON.parse(httpRequest.responseText);
    }
 }
 
