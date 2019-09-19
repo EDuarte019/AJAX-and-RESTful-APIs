d239d82d48864ea589faf59c80bf9e2cd88a7f4f
Evelyn <eduarte019@west-mec.org>
2019-09-18 18:09:53 -0700

    Final commit
    The exercise is complete

--- 02-AJAX/Exercise-LAMP/www/scripts.js
+++ 02-AJAX/Exercise-LAMP/www/scripts.js
@@ -49,13 +49,11 @@ function getWeather(evt) {
 
    if (!httpRequest) {
       httpRequest = getRequestObject();
+      httpRequest.abort();
+      httpRequest.open("get", "solar.php?" + "lat=" + "&lng=" + longitude, true);
+      httpRequest.send(null);
+      httpRequest.onreadystatechange = fillWeather;
    }
-
-   httpRequest.abort();
-   httpRequest.open("get", "solar.php?" + "lat=" + latitude + "&lng=" + longitude, true);
-   httpRequest.send(null);
-   httpRequest.onreadystatechange = fillWeather;
-
 }
 
 function fillWeather() {
@@ -71,21 +69,55 @@ function fillWeather() {
       document.querySelector("section.week table caption").innerHTML = selectedCity;
 
       document.querySelector("section.week table caption ").style.display = "block ";
-         document.querySelector("section.week table ").style.display = "inline - block ";
+      document.querySelector("section.week table ").style.display = "inline - block ";
+      for (var i = 0; i < rows.length; i++) {
+         var firstCell = rows[i].getElementsByTagName("td")[0];
+         var secondCell = rows[i].getElementsByTagName("td")[1];
+         var thirdCell = rows[i].getElementsByTagName("td")[2];
+         firstCell.innerHTML = days[daysOfWeek];
+         if (daysOfWeek + 1 === 7) {
+            daysOfWeek = 0;
+         } else {
+            daysOfWeek++;
          }
-      }
-
-      var locations = document.querySelectorAll("section ul li");
-
-      for (var i = 0; i < locations.length; i++) {
-         if (locations[i].addEventListener) {
-            locations[i].addEventListener("click", getWeather, false);
-         } else if (locations[i].attachEvent) {
-            locations[i].attachEvent("onclick", getWeather);
+         var sun = Math.round((1 - weatherReport.daily.data[i].cloudCover) * 100, 0)
+         if (sun > 90) {
+            secondCell.style.color = "rgb(255, 171,0)";
+         } else if (sun > 80 && sun <= 90) {
+            secondCell.style.color = "rgb(255, 179,25)";
+         } else if (sun > 70 && sun <= 80) {
+            secondCell.style.color = "rgb(255,188,51)";
+         } else if (sun > 60 && sun <= 70) {
+            secondCell.style.color = "rgb(255,196,77)";
+         } else if (sun > 50 && sun <= 60) {
+            secondCell.style.color = "rgb(255,205,102)";
+         } else if (sun > 40 && sun <= 50) {
+            secondCell.style.color = "rgb(255,213,128)";
+         } else if (sun > 30 && sun <= 40) {
+            secondCell.style.color = "rgb(255,221,153)";
+         } else if (sun > 20 && sun <= 30) {
+            secondCell.style.color = "rgb(255,230,179)";
+         } else if (sun > 10 && sun <= 20) {
+            secondCell.style.color = "rgb(255,238,204)";
+         } else if (sun <= 10) {
+            secondCell.style.color = "rgb(255,247,230)";
          }
+         secondCell.style.fontSize = "2.5em";
+         thirdCell.innerHTML = sun + "%";
       }
-      if (window.addEventListener) {
-         window.addEventListener("load", getWeather, false);
-      } else if (window.attachEvent) {
-         window.attachEvent("onload", getWeather);
-      }
+   }
+   document.querySelector("section.week p.credit").style.display = "block";
+}
+var locations = document.querySelectorAll("section ul li");
+for (var i = 0; i < locations.length; i++) {
+   if (locations[i].addEventListener) {
+      locations[i].addEventListener("click", getWeather, false);
+   } else if (locations[i].attachEvent) {
+      locations[i].attachEvent("onclick", getWeather);
+   }
+}
+if (window.addEventListener) {
+   window.addEventListener("load", getWeather, false);
+} else if (window.attachEvent) {
+   window.attachEvent("onload", getWeather);
+}
